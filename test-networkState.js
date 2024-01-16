5f170863ec0fe455940a13900be35d56383de2a3
Justin Donnelly <justinrdonnelly@gmail.com>
2024-01-16 11:14:09 -0500

    Test that necessary information is accessible
    Since we don't have access to EventEmitter in the test code, we just use
    setTimeout to wait 1 second before reading the information.

--- test-networkState.js
+++ test-networkState.js
@@ -1,8 +1,18 @@
 import GLib from 'gi://GLib';
 import {NetworkState} from './networkState.js';
 
+const networkState = new NetworkState();
 
-new NetworkState();
+setTimeout(() => {
+    // console.log(`networkManager: ${networkState.networkManager}`);
+    // console.log(`networkDevices: ${networkState.networkManager.networkDevices}`);
+    const devices = networkState.networkManager.networkDevices;
+    devices.forEach(device => {
+        // console.log(`Connection: ${device.connection}`);
+        console.log(`Connection ID: ${device.connection.activeConnectionId}`);
+        console.log(`Settings object path: ${device.connection.activeConnectionSettings}`);
+    });
+}, 1000);
 
 const loop = GLib.MainLoop.new(null, false);
 loop.run();
