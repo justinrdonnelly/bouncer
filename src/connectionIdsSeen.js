c1c55c85d19f640fd787d2953aa58a45dfa150b9
Justin Donnelly <justinrdonnelly@gmail.com>
2025-05-26 20:32:22 -0400

    Move file I/O to its own class
    Instead of everything being embedded in `connectionIdsSeen.js`. This
    will make it easier to reuse this functionality.

--- src/connectionIdsSeen.js
+++ src/connectionIdsSeen.js
@@ -9,49 +9,21 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-import Gio from 'gi://Gio';
-import GLib from 'gi://GLib';
+import { Data } from './data.js';
 
 export class ConnectionIdsSeen {
-    static #dataDirectoryPermissions = 0o700; // gnome-control-center uses 700 (USER_DIR_MODE), so we'll do that too
-    static #textFormat = 'utf-8';
     #connectionIdsSeen;
+    #data;
     #fileName = 'connection-ids-seen.json';
-    #destination;
-    #destinationFile;
-    #destinationDirectory;
 
     constructor() {
-        const dataDir = GLib.get_user_data_dir();
-        this.#destination = GLib.build_filenamev([dataDir, 'bouncer', this.#fileName]);
-        this.#destinationFile = Gio.File.new_for_path(this.#destination);
-        this.#destinationDirectory = this.#destinationFile.get_parent().get_path();
+        this.#data = new Data(this.#fileName);
     }
 
     // Always call init immediately after constructor.
     async init() {
-        if (GLib.mkdir_with_parents(this.#destinationDirectory, ConnectionIdsSeen.#dataDirectoryPermissions) !== 0)
-            // mkdir failed
-            throw new Error(`Cannot create directory ${this.#destinationDirectory}`);
-        this.#connectionIdsSeen = await this.#createConnectionIdsSeen();
-    }
-
-    async #createConnectionIdsSeen() {
-        try {
-            // eslint-disable-next-line no-unused-vars
-            const [contents, etag] = await this.#destinationFile.load_contents_async(null);
-            const decoder = new TextDecoder(ConnectionIdsSeen.#textFormat);
-            const decoded = decoder.decode(contents);
-            return JSON.parse(decoded);
-        } catch (e) {
-            if (e.message.includes('No such file or directory')) {
-                // file does not yet exist, that's OK
-                console.log(`File ${this.#destination} does not exist. Treat as empty and create it later.`);
-                return [];
-            }
-            else
-                throw e;
-        }
+        this.#data.init();
+        this.#connectionIdsSeen = await this.#data.getData();
     }
 
     isConnectionNew(connectionId) {
@@ -68,28 +40,8 @@ export class ConnectionIdsSeen {
 
     async syncConnectionIdToSeen() {
         // Don't try/catch here. Allow it to propagate.
-        console.log(`Syncing in memory representation to ${this.#fileName}.`);
         const dataJSON = JSON.stringify(this.#connectionIdsSeen);
-        const encoder = new TextEncoder(ConnectionIdsSeen.#textFormat);
-        const encodedData = encoder.encode(dataJSON);
-        // We already tried to create this directory earlier, so this should only matter if a user somehow deleted
-        // it.
-        if (GLib.mkdir_with_parents(
-            this.#destinationDirectory, ConnectionIdsSeen.#dataDirectoryPermissions) === 0) {
-            // Since we `await` the results, we do not need to use `replace_contents_bytes_async`
-            let success = await this.#destinationFile.replace_contents_async(
-                encodedData,
-                null,
-                false,
-                Gio.FileCreateFlags.REPLACE_DESTINATION,
-                null
-            );
-            if (!success) {
-                throw new Error(`Error saving data file ${this.#destination}.`);
-            }
-        } else {
-            throw new Error(`Error creating directory ${this.#destinationDirectory}.`);
-        }
+        this.#data.saveData(dataJSON);
     }
 
 }
---
+++ src/data.js
@@ -0,0 +1,79 @@
+/* data.js
+ *
+ * Copyright 2025 Justin Donnelly
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+
+export class Data {
+    static #dataDirectoryPermissions = 0o700; // gnome-control-center uses 700 (USER_DIR_MODE), so we'll do that too
+    static #textFormat = 'utf-8';
+    #fileName;
+    #destination;
+    #destinationFile;
+    #destinationDirectory;
+
+    constructor(fileName) {
+        this.#fileName = fileName;
+        const dataDir = GLib.get_user_data_dir();
+        this.#destination = GLib.build_filenamev([dataDir, 'bouncer', this.#fileName]);
+        this.#destinationFile = Gio.File.new_for_path(this.#destination);
+        this.#destinationDirectory = this.#destinationFile.get_parent().get_path();
+    }
+
+    // Always call init immediately after constructor.
+    async init() {
+        if (GLib.mkdir_with_parents(this.#destinationDirectory, Data.#dataDirectoryPermissions) !== 0)
+            // mkdir failed
+            throw new Error(`Cannot create directory ${this.#destinationDirectory}`);
+    }
+
+    async getData() {
+        try {
+            // eslint-disable-next-line no-unused-vars
+            const [contents, etag] = await this.#destinationFile.load_contents_async(null);
+            const decoder = new TextDecoder(Data.#textFormat);
+            const decoded = decoder.decode(contents);
+            return JSON.parse(decoded);
+        } catch (e) {
+            if (e.message.includes('No such file or directory')) {
+                // file does not yet exist, that's OK
+                console.log(`File ${this.#destination} does not exist. Treat as empty and create it later.`);
+                return [];
+            }
+            else
+                throw e;
+        }
+    }
+
+    async saveData(data) {
+        // Don't try/catch here. Allow errors to propagate.
+        console.log(`Saving data to ${this.#fileName}.`);
+        const encoder = new TextEncoder(Data.#textFormat);
+        const encodedData = encoder.encode(data);
+        // We already tried to create this directory earlier, so this should only matter if a user somehow deleted it.
+        if (GLib.mkdir_with_parents(
+            this.#destinationDirectory, Data.#dataDirectoryPermissions) === 0) {
+            // Since we `await` the results, we do not need to use `replace_contents_bytes_async`
+            let success = await this.#destinationFile.replace_contents_async(
+                encodedData,
+                null,
+                false,
+                Gio.FileCreateFlags.REPLACE_DESTINATION,
+                null
+            );
+            if (!success) {
+                throw new Error(`Error saving data file ${this.#destination}.`);
+            }
+        } else {
+            throw new Error(`Error creating directory ${this.#destinationDirectory}.`);
+        }
+    }
+}
