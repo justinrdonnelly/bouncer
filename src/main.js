40aea19c23bfdfb75e5fd90c3f38e2f2a02b331f
Justin Donnelly <justinrdonnelly@gmail.com>
2025-10-11 18:21:28 -0400

    Run dependency checks even if this.#dependencyCheck already exists
    We'll now be instantiating DependencyCheck in additional situations, so
    it's possible it will already be instantiated when #monitorNetwork is
    called. But we'll still want to do the actual checks.
    
    We also pulled the instantiation of DependencyCheck into its own method.

--- src/main.js
+++ src/main.js
@@ -96,20 +96,13 @@ export const BouncerApplication = GObject.registerClass(
             this.#dashboardWindow = null;
         }
 
-        // The monitorNetwork method will instantiate NetworkState and listen for its signals.
-        async #monitorNetwork() {
-            if (this.#monitoring) {
-                return;
-            }
-            this.hold();
-            this.#monitoring = true;
+        #instantiateDependencyCheck() {
             try {
                 if (this.#dependencyCheck === null) {
                     this.#dependencyCheck = new DependencyCheck();
-                    this.#dependencyCheck.connect('error', this.#handleErrorSignal.bind(this));
-                    this.#dependencyCheck.connect('first-run-setup-complete', this.#handleFirstRunSignal.bind(this));
-                    await this.#dependencyCheck.runChecks();
                 }
+                this.#dependencyCheck.connect('error', this.#handleErrorSignal.bind(this));
+                this.#dependencyCheck.connect('first-run-setup-complete', this.#handleFirstRunSignal.bind(this));
             } catch (e) {
                 // This should really never happen. DependencyCheck is full of `try/catch`es, so exceptions shouldn't
                 // get this far. Since we don't know how this happened, we'll log it, continue, and hope for the best.
@@ -123,6 +116,17 @@ export const BouncerApplication = GObject.registerClass(
                         'information.')
                 );
             }
+        }
+
+        // The monitorNetwork method will instantiate NetworkState and listen for its signals.
+        async #monitorNetwork() {
+            if (this.#monitoring) {
+                return;
+            }
+            this.hold();
+            this.#monitoring = true;
+            this.#instantiateDependencyCheck();
+            await this.#dependencyCheck.runChecks();
             try {
                 if (this.#connectionIdsSeen === null) {
                     this.#connectionIdsSeen = new ConnectionIdsSeen();
