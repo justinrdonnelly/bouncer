0fb815a4cba2177d5231479d325aa6b6f830d528
Justin Donnelly <justinrdonnelly@gmail.com>
2026-01-31 17:11:08 -0500

    Track connection IDs seen per machine
    Previously we just kept a list of connection IDs. This works as long as
    you don't take this list to a new machine (where you won't have your
    previous NetworkManager configuration). But since flatpak data files are
    kept in the home directory, and some users bring their entire home
    directory to a new machine or installation, they could run into
    problems. For connections seen on a previous machine, Bouncer would not
    prompt for the firewall zone, but NetworkManager wouldn't set the zone.
    
    We solve this by tracking the connections for a given machine ID. "[The
    machine ID is usually generated from a random source during system
    installation or first boot and stays constant for all subsequent
    boots.][1]" This will force Bouncer to prompt again for a network if it
    was not seen on the current machine.
    
    [1]: https://www.freedesktop.org/software/systemd/man/latest/machine-id.html

--- src/connectionIdsSeen.js
+++ src/connectionIdsSeen.js
@@ -9,11 +9,15 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+
 import { Data } from './data.js';
 
 export class ConnectionIdsSeen {
     static #fileName = 'connection-ids-seen.json';
-    #connectionIdsSeen;
+    #connectionIdsSeen; // array of connection IDs for this machine
+    #allConnectionIdsSeen; // map of machine ID to array of connection IDs for the machine
     #data;
 
     constructor() {
@@ -22,11 +26,31 @@ export class ConnectionIdsSeen {
 
     // Always call init immediately after constructor.
     async init() {
-        const data = await this.#data.getData();
-        if (data === null)
+        // Don't try/catch here. Allow errors to propagate.
+        const [data, machineId] = await Promise.all([this.#data.getData(), this.getMachineId()]);
+
+        if (data === null) {
             this.#connectionIdsSeen = [];
-        else
-            this.#connectionIdsSeen = JSON.parse(data);
+            this.#allConnectionIdsSeen = {[machineId]: this.#connectionIdsSeen};
+        } else {
+            this.#allConnectionIdsSeen = JSON.parse(data);
+            // We'll have to migrate existing setups from purely array based to an object, where the key is the machine
+            // ID, and the value is the array of connection IDs.
+            // This code is temporary, and should be removed after we figure users have migrated to the new format.
+            if (Array.isArray(this.#allConnectionIdsSeen)) { // It's an array (old format). Convert it.
+                console.log(`Migrating data format for ${ConnectionIdsSeen.#fileName}`);
+                this.#connectionIdsSeen = this.#allConnectionIdsSeen;
+                this.#allConnectionIdsSeen = {[machineId]: this.#connectionIdsSeen};
+                // save it to update the format of the saved data
+                await this.save();
+            } else {
+                const newMachine = !Object.hasOwn(this.#allConnectionIdsSeen, machineId);
+                if (newMachine) {
+                    this.#allConnectionIdsSeen[machineId] = [];
+                }
+                this.#connectionIdsSeen = this.#allConnectionIdsSeen[machineId];
+            }
+        }
     }
 
     isConnectionNew(connectionId) {
@@ -43,8 +67,34 @@ export class ConnectionIdsSeen {
 
     async save() {
         // Don't try/catch here. Allow errors to propagate.
-        const dataJSON = JSON.stringify(this.#connectionIdsSeen);
+        const dataJSON = JSON.stringify(this.#allConnectionIdsSeen);
         this.#data.saveData(dataJSON);
+    }
+
+    async getMachineId() {
+        return new Promise((resolve, reject) => {
+            Gio.DBus.session.call(
+                'io.github.justinrdonnelly.bouncer',
+                '/io/github/justinrdonnelly/bouncer',
+                'org.freedesktop.DBus.Peer',
+                'GetMachineId',
+                null,
+                new GLib.VariantType('(s)'), // reply type
+                Gio.DBusCallFlags.NONE,
+                -1, // timeout
+                null, // cancellable
+                (connection, res) => {
+                    try {
+                        const reply = connection.call_finish(res);
+                        resolve(reply.get_child_value(0).recursiveUnpack());
+                    } catch (e) {
+                        if (e instanceof Gio.DBusError)
+                            Gio.DBusError.strip_remote_error(e);
+                        reject(e);
+                    }
+                }
+            );
+        });
     }
 
 }
